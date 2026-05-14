15f9102b945add96c550126a185fdedf3be8edd9
younisch96 <younis.ch96@gmail.com>
2026-05-15 01:01:17 +0500

    fix: set basePath=/editor for reverse proxy support

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: false,
+	basePath: '/editor',
 	allowedDevOrigins: ['192.168.50.216', '*.local', 'localhost', 'vh.tomtap.ai', '*.tomtap.ai'],
 };
 
